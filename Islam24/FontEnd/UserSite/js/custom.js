9beb87d3b496cb2c18bb729c41c98ecd5da0bfa4
Pranto-275 <67626492+Pranto-275@users.noreply.github.com>
2021-09-09 21:55:37 +0600

    homepage
    homepage for islam 24

---
+++ Islam24/FontEnd/UserSite/js/custom.js
@@ -0,0 +1,73 @@
+// Owl Carousel Start..................
+
+
+
+$(document).ready(function() {
+    var one = $("#one");
+    var two = $("#two");
+
+    $('#customNextBtn').click(function() {
+        one.trigger('next.owl.carousel');
+    })
+    $('#customPrevBtn').click(function() {
+        one.trigger('prev.owl.carousel');
+    })
+    one.owlCarousel({
+        autoplay: true,
+        loop: true,
+        dot: true,
+        autoplayHoverPause: true,
+        autoplaySpeed: 100,
+        margin: 10,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+
+    two.owlCarousel({
+        autoplay: true,
+        loop: true,
+        dot: true,
+        autoplayHoverPause: true,
+        autoplaySpeed: 100,
+        margin: 10,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+});
+
+
+
+
+
+
+
+
+// Owl Carousel End..................
+
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
